c8ad5419a5974e417a16d378788b5db1062041ea
P. Christopher Bowers <pcbowers@gmail.com>
2021-10-18 16:37:13 -0400

    created base renderer (no functionality)

---
+++ src/routes/Renderer.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { Options, CustomOptions } from '$lib/renderTypes';
+	export let options: Options | CustomOptions;
+
+	console.log(options);
+</script>
+
+<h1>Welcome to Segments</h1>
+<p>
+	Visit <a href="https://github.com/pcbowers/segments">pcbowers/segments</a> to read the documentation
+</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,15 @@
-<h1>Welcome to Segments</h1>
-<p>
-	Visit <a href="https://github.com/pcbowers/segments">pcbowers/segments</a> to read the documentation
-</p>
+<script lang="ts">
+	import type { Options } from '$lib/renderTypes';
+	import Renderer from './Renderer.svelte';
+
+	let rendererOptions: Options = {
+		segments: [
+			{
+				_id: 'asdf',
+				_type: 'horizontalrule'
+			}
+		]
+	};
+</script>
+
+<Renderer options={rendererOptions} />
